715ff2449c7bb1b29897d3735f798bb79d545138
Pandicon <70060103+Pandicon@users.noreply.github.com>
2021-12-02 23:17:10 +0100

    Add solution of path 1

---
+++ 2021/Day-1/Part-1/depth-measurement.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+
+function readInput(path = "input.txt", encoding = "utf8", flag = "r") {
+	return fs.readFileSync(path, { encoding, flag });
+};
+
+function saveOutput(data, path = "output.txt", encoding = "utf8", flag = "w") {
+	fs.writeFileSync(path, data, { encoding, flag });
+}
+
+const input = readInput();
+const numArr = input.split("\n").map(x => parseInt(x));
+let increasedAmount = 0;
+for(let i = 1; i < numArr.length; i++) {
+	if(numArr[i-1] < numArr[i]) increasedAmount++;
+};
+saveOutput(increasedAmount.toString());
