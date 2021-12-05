14644b6fc0fad70ca21b986a50f1a776dd4b75ba
Pandicon <70060103+Pandicon@users.noreply.github.com>
2021-12-05 17:51:27 +0100

    (2020/6/2) Shorten the solution quite a bit

--- 2020/Day-6/Part-2/solution.js
+++ 2020/Day-6/Part-2/solution.js
@@ -13,20 +13,8 @@ const groups = input.split(/\r/).join("").split("\n\n");
 const answersPeople = groups.map(group => group.split(/\n/).map(person => person.split("")));
 let totalAnswers = 0;
 for(const groupAnswers of answersPeople) {
-	let all = [];
 	const firstPerson = groupAnswers[0];
-	for(const answer of firstPerson) {
-		if(!all.includes(answer)) all.push(answer);
-	};
-	for(const person of groupAnswers) {
-		let currAll = all.slice();
-		for(const answer of all) {
-			if(!person.includes(answer) && currAll.includes(answer)) currAll.splice(currAll.indexOf(answer), 1);
-			if(currAll.length == 0) break;
-		};
-		all = currAll.slice();
-		if(all.length == 0) break;
-	};
+	const all = firstPerson.filter(answer => groupAnswers.every(personAnswers => personAnswers.includes(answer)));
 	totalAnswers += all.length;
 };
 saveOutput(totalAnswers.toString());
