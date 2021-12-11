99a438a56f0314090d60ef3920369a4175647e10
Pandicon <70060103+Pandicon@users.noreply.github.com>
2021-12-11 06:34:49 +0100

    (2021/11/02) Add solution

---
+++ 2021/Day-11/Part-2/solution.js
@@ -0,0 +1,54 @@
+const fs = require('fs');
+
+function readInput(path = "input.txt", encoding = "utf8", flag = "r") {
+	return fs.readFileSync(path, { encoding, flag });
+};
+
+function saveOutput(data, path = "output.txt", encoding = "utf8", flag = "w") {
+	fs.writeFileSync(path, data, { encoding, flag });
+};
+
+const input = readInput();
+const lines = input.split(/\r/).join("").split("\n");
+const octopuses = input.split(/\r/).join("").split("\n").join("").split("").map(Number);
+const octopusesOnLine = lines[0].length;
+let result = 0;
+let day = 1;
+while(!result) {
+	let flashed = [];
+	for(let j = 0; j < octopuses.length; j++) {
+		octopuses[j] += 1;
+	}
+	let flash = octopuses.findIndex(function(number) {
+		return number > 9;
+	});
+	while(flash > -1) {
+		if(flashed.includes(flash)) {
+			octopuses[flash] = 0;
+			flash = octopuses.findIndex(function(number) {
+				return number > 9;
+			});
+			continue;
+		}
+		const neighbours = [
+			flash-octopusesOnLine,
+			flash+octopusesOnLine
+		];
+		if(flash % octopusesOnLine != 0) neighbours.push(flash-octopusesOnLine-1, flash-1, flash+octopusesOnLine-1);
+		if(flash % octopusesOnLine != octopusesOnLine-1) neighbours.push(flash-octopusesOnLine+1, flash+1, flash+octopusesOnLine+1);
+		for(const neighbour of neighbours) {
+			if(neighbour < 0 || neighbour >= octopuses.length) continue;
+			octopuses[neighbour] += 1;
+		};
+		flashed.push(flash);
+		flash = octopuses.findIndex(function(number) {
+			return number > 9;
+		});
+	}
+	for(const i of flashed) octopuses[i] = 0;
+	if(flashed.length == octopuses.length) {
+		result = day;
+	};
+	day += 1;
+}
+saveOutput(result.toString());
