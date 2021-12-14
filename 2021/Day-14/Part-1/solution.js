09320822824c6f25dd343ff7e0d48c36009a3099
Pandicon <70060103+Pandicon@users.noreply.github.com>
2021-12-14 20:28:32 +0100

    (2021/14/1) Update the solution to be much more optimised

--- 2021/Day-14/Part-1/solution.js
+++ 2021/Day-14/Part-1/solution.js
@@ -8,6 +8,8 @@ function saveOutput(data, path = "output.txt", encoding = "utf8", flag = "w") {
 	fs.writeFileSync(path, data, { encoding, flag });
 };
 
+const insertNew = (object, key, value = 0) => object[key] = ((key in object) ? object[key] + value : value);
+
 const endOfLine = require("os").EOL;
 
 const input = readInput();
@@ -20,30 +22,26 @@ for(const rule of rulesArr) {
 	rules[rule.split(" -> ")[0]] = rule.split(" -> ")[1];
 }
 
-for(let i = 0; i < 10; i++) {
-	const currPolymer = polymer;
-	let newPolymer = [currPolymer[0]];
-	for(let i = 1; i < currPolymer.length; i++) {
-		let pair = `${currPolymer[i-1]}${currPolymer[i]}`;
-		if(rules[pair]) {
-			newPolymer.push(rules[pair]);
-		}
-		newPolymer.push(currPolymer[i]);
+let vals = {};
+for (let i = 0; i < polymer.length - 1; i++) insertNew(vals, polymer[i] + polymer[i+1], 1);
+
+for (let i = 0, result = {}; i < 10; i++, result = {}){
+    for (const val in vals) {
+		insertNew(result, val[0]+rules[val], vals[val]);
+		insertNew(result, rules[val]+val[1], vals[val]);
 	}
-	polymer = newPolymer.join("");
-}
-let min = [polymer.length, ""];
-let max = [0, ""];
-const letterCounts = {};
-for(let i = 0; i < polymer.length; i++) {
-	const letter = polymer[i];
-	if(!letterCounts[letter]) letterCounts[letter] = 0;
-	letterCounts[letter] += 1;
-	if(letterCounts[letter] > max[0]) max = [letterCounts[letter], letter];
-}
-for(const letter in letterCounts) {
-	if(letterCounts[letter] < min[0]) min = [letterCounts[letter], letter];
+    vals = result;
 }
 
-const result = max[0] - min[0];
+let resVals = {};
+for(const key in vals) {
+	insertNew(resVals, key[0], vals[key]/2);
+	insertNew(resVals, key[1], vals[key]/2);
+}
+resVals[polymer[0]] += 0.5;
+resVals[polymer[polymer.length - 1]] += 0.5;
+
+const sorted = Object.values(resVals).sort((a, b) => b - a);
+
+const result = sorted[0] - sorted[sorted.length-1];
 saveOutput(result.toString());
