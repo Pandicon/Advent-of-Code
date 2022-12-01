4bea718af2a0c2c899dbfd41d5d41ffce5cb3340
Pandicon <70060103+Pandicon@users.noreply.github.com>
2022-12-01 17:04:03 +0100

    (2022-01-02) Add support for Windows ends of lines

--- 2022/Day-01/Part-02/src/main.rs
+++ 2022/Day-01/Part-02/src/main.rs
@@ -2,7 +2,7 @@ fn main() {
 	let env_vars: Vec<String> = std::env::args().collect();
 	let run_example = env_vars.len() > 1 && env_vars[1].to_lowercase() == "example";
 	let input_path = if run_example { "./example-input.txt" } else { "./input.txt" };
-	let input = std::fs::read_to_string(input_path).expect("Should have been able to read the file");
+	let input = std::fs::read_to_string(input_path).expect("Should have been able to read the file").replace("\r\n", "\n");
 
 	let mut elves_calories: Vec<i32> = input
 		.split("\n\n")
