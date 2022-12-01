73fab1e330a131cd10fbe7711847b2e6a5b2459b
Pandicon <70060103+Pandicon@users.noreply.github.com>
2022-12-01 07:03:45 +0100

    (2022-01-01) Add my solution

---
+++ 2022/Day-01/Part-01/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+	let env_vars: Vec<String> = std::env::args().collect();
+	let run_example = env_vars.len() > 1 && env_vars[1].to_lowercase() == "example";
+	let input_path = if run_example { "./example-input.txt" } else { "./input.txt" };
+	let input = std::fs::read_to_string(input_path).expect("Should have been able to read the file");
+
+	let max_calories = input
+		.split("\n\n")
+		.map(|elf_inv| elf_inv.split("\n").map(|snack_calories| snack_calories.trim().parse::<i32>().unwrap()).sum::<i32>())
+		.max()
+		.unwrap();
+	println!("{}", max_calories);
+}
