803c96aaa4a3714616bd16bd1e4788419978b45d
Pandicon <70060103+Pandicon@users.noreply.github.com>
2024-12-10 12:28:51 +0100

    (2024-10-02) Add my solution

---
+++ 2024/Day-10/Part-02/src/main.rs
@@ -0,0 +1,57 @@
+fn main() {
+	let input = std::fs::read_to_string("./input.txt").expect("Should have been able to read the file").replace("\r\n", "\n");
+	let res = solve(input);
+	println!("{res}");
+}
+
+fn solve(input: String) -> usize {
+	let heights = input.trim().split('\n').map(|l| l.trim()).filter(|l| !l.is_empty()).map(|l| l.chars().map(|c| c.to_digit(10).unwrap() as u8).collect()).collect::<Vec<Vec<u8>>>();
+	let mut zeros = Vec::new();
+	for y in 0..heights.len() {
+		for x in 0..heights[y].len() {
+			if heights[y][x] == 0 {
+				zeros.push([x, y]);
+			}
+		}
+	}
+	let mut res = 0;
+	for [x_0, y_0] in zeros {
+		let mut queue = vec![[x_0, y_0]];
+		while let Some([x, y]) = queue.pop() {
+			let val = heights[y][x];
+			if val == 9 {
+				res += 1;
+				continue;
+			}
+			// Left
+			if x > 0 && heights[y][x - 1] == val + 1 {
+				queue.push([x - 1, y]);
+			}
+			// Up
+			if y > 0 && heights[y - 1][x] == val + 1 {
+				queue.push([x, y - 1]);
+			}
+			// Right
+			if x < heights[y].len() - 1 && heights[y][x + 1] == val + 1 {
+				queue.push([x + 1, y]);
+			}
+			// Down
+			if y < heights.len() - 1 && heights[y + 1][x] == val + 1 {
+				queue.push([x, y + 1]);
+			}
+		}
+	}
+	res
+}
+
+#[cfg(test)]
+mod tests {
+	use super::solve;
+
+	#[test]
+	fn example() {
+		let input = std::fs::read_to_string("./example-input.txt").expect("Should have been able to read the file").replace("\r\n", "\n");
+		let res = solve(input);
+		assert_eq!(res, 81);
+	}
+}
