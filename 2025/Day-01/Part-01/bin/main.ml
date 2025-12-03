18921c9b256e9076f69fab33128f33c29c560001
Pandicon <70060103+Pandicon@users.noreply.github.com>
2025-12-03 22:44:58 +0000

    (2025-01-01) Add inputs and my solution

---
+++ 2025/Day-01/Part-01/bin/main.ml
@@ -0,0 +1,43 @@
+let run_example = false
+
+let read_file (file_name : string) : string =
+  In_channel.with_open_bin file_name In_channel.input_all
+
+let remove_char_occurences (s : string) (c_to_remove : char) : string =
+  s |> String.to_seq |> Seq.filter (fun c -> c <> c_to_remove) |> String.of_seq
+
+let read_input_full_file : string =
+  let input_raw = read_file (if run_example then "example-input.txt" else "input.txt")
+  in
+  remove_char_occurences input_raw '\r'
+
+let modulo (x : int) (n : int) : int =
+  let rem = x mod n in
+  if rem >= 0 then rem
+  else rem + n
+
+let solve (input : string) : int =
+  let modulo_n = 100 and start_pos = 50 in
+  let lines = String.split_on_char '\n' input
+  in
+  let rotate (position : int) (delta : int) (score : int) : (int * int) =
+    let new_pos = modulo (position + delta) (modulo_n) in
+    (new_pos, score + (if new_pos = 0 then 1 else 0))
+  in
+  let rec inner (rows_to_handle : string list) (position : int) (score : int) : int =
+    match rows_to_handle with
+    | [] -> score
+    | row :: rest ->
+      let direction = String.sub row 0 1 and delta = int_of_string (String.sub row 1 ((String.length row) - 1)) in
+      let delta = if direction = "L" then -delta else delta in
+      let (new_pos, new_score) = rotate position delta score in
+      inner rest new_pos new_score
+  in inner lines start_pos 0
+
+let main =
+  let input = read_input_full_file
+  in
+  solve input
+
+let () =
+  print_endline (string_of_int main)
